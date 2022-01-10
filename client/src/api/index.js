ae1030dd8672e8b6a757b2636843d95784b2548b
warmachine028 <warmachine028@outlook.com>
2022-01-10 16:58:16 +0530

    Added changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
-// const url = "https://memories-pritam.herokuapp.com/posts";
-API.interceptors.request.use((req) => { 
-    if (localStorage.getItem('profile'))
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    return req;    
+const API = axios.create({ baseURL: "https://memories-pritam.herokuapp.com" });
+
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem("profile"))
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem("profile")).token
+		}`;
+	return req;
 });
 
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const updatePost = (id, updatedPost) =>
+	API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
--- server/index.js
+++ server/index.js
@@ -17,13 +17,11 @@ app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
 app.get('/', (req, res) => { res.send("Hello to Memories API") });
-
-const CONNECTION_URL = process.env.CONNECTION_URL;
 	
 const PORT = process.env.PORT || 5000;
 
 // https://www.mongodb.com/cloud/atlas
 mongoose
-	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) => console.log(`${error} did not connect`));
