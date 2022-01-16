14149c2e4feba4e676813bc72e24c95e9328901d
warmachine028 <warmachine028@outlook.com>
2022-01-16 12:44:27 +0530

    Added Pagination and Post Details

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,10 +5,39 @@ import PostMessage from "../models/postMessage.js";
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
-	try {
-		const postMessages = await PostMessage.find();
+	const { page } = req.query;
 
-		res.status(200).json(postMessages);
+	try {
+		const LIMIT = 8;
+		const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+		const total = await PostMessage.countDocuments({});
+		const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+		res.status(200).json({data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
+export const getPost = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const post = await PostMessage.findById(id);
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
+
+export const getPostsBySearch = async (req, res) => {
+	const { searchQuery, tags } = req.query;
+	try {
+		const title = new RegExp(searchQuery, 'i'); 
+		const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(',') } }] });
+
+		res.status(200).json({ data: posts });
 	} catch (error) {
 		res.status(404).json({ message: error.message });
 	}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // http://localhost:5000/posts/
-
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
