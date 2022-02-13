39708225658512431287c175976cf54dd7c8549c
warmachine028 <warmachine028@outlook.com>
2022-02-13 23:38:45 +0530

    Resolved vulnerabilities

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,6 @@
-export default {
+module.export = {
     env: { browser: true, es6: true },
-    extends: ["plugin:react/recommended", "airbnb"],
+    extends: ["plugin:react/recommended", "airbnb-base"],
     globals: { Atomics: "readonly", SharedArrayBuffer: "readonly" },
     parserOptions: {
         ecmaFeatures: { jsx: true },
