18125c98789ebca1decb684058e160034b22cb00
warmachine028 <warmachine028@outlook.com>
2022-07-12 13:15:12 +0530

    added fix

--- server/index.js
+++ server/index.js
@@ -6,14 +6,11 @@ import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/user.js'
 
-const app = express()
-app.use(
-	cors({
-		origin: 'https://memories-pritam.netlify.app',
-	})
-)
-
 dotenv.config()
+
+const app = express()
+
+app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
