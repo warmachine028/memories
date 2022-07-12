8a6e8e92d04246ae4a1c14d1ca9f99b95b03ca7e
warmachine028 <warmachine028@outlook.com>
2022-07-12 15:01:16 +0530

    added fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 import './index.css'
 import App from './App/App'
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)))
 ReactDOM.render(
@@ -15,8 +15,7 @@ ReactDOM.render(
 	document.getElementById('root')
 )
 
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register()
