31563c078169e0243f614bbccf565fe0064fdc80
warmachine028 <warmachine028@outlook.com>
2023-03-08 21:19:13 +0530

    feat: added backend logic for getPost, getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,11 +4,13 @@ import Post from '../models/post.js'
 
 const router = express.Router()
 
+const setCreator = (posts) => posts.map((post) => ({ ...post, creator: post.creator[0] }))
+
 export const getPosts = async (req, res) => {
 	const { page } = req.query
 
 	try {
-		const userId = mongoose.Types.ObjectId(req.userId)
+		const userId = mongoose.Types.ObjectId(req.userId?.padStart(24, '0'))
 		const query = { $or: [{ creator: userId }, { private: false }] }
 		const LIMIT = 8
 		const total = await Post.countDocuments(query)
@@ -29,11 +31,12 @@ export const getPosts = async (req, res) => {
 				},
 			},
 		])
-		posts = posts.map((post) => ({ ...post, creator: post.creator[0] }))
+		posts = setCreator(posts)
 		const end = Date.now()
 		console.log(`Fetching took ${(end - start) / 1000} seconds`)
 		res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) })
 	} catch (error) {
+		console.log(error)
 		res.status(404).json({ message: error.message })
 	}
 }
@@ -41,9 +44,25 @@ export const getPosts = async (req, res) => {
 export const getPost = async (req, res) => {
 	const { id } = req.params
 	try {
-		const post = await Post.findById(id)
-		if (!post || (post._private && post.creator !== req.userId)) return res.status(404).send('No post with that id')
-
+		let posts = await Post.aggregate([
+			{ $match: { _id: mongoose.Types.ObjectId(id) } },
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator',
+					foreignField: '_id',
+					as: 'creator',
+				},
+			},
+		])
+		posts = setCreator(posts)
+		if (!posts.length) {
+			return res.status(404).send('No post with that id')
+		}
+		const post = posts[0]
+		if (post.private && `${post.creator._id}` !== req.userId) {
+			return res.status(404).send("Can't access post with that Id")
+		}
 		res.status(200).json(post)
 	} catch (error) {
 		res.status(404).json({ message: error.message })
@@ -54,17 +73,30 @@ export const getPostsBySearch = async (req, res) => {
 	const { searchQuery, tags } = req.query
 	try {
 		const title = new RegExp(searchQuery, 'i')
+		const userId = mongoose.Types.ObjectId(req.userId?.padStart(24, '0'))
 		const query = {
 			$and: [
 				{
-					$or: [{ creator: req.userId }, { _private: false }, { _private: { $exists: false } }],
+					$or: [{ creator: userId }, { private: false }],
 				},
 				{
 					$or: [{ title }, { tags: { $in: tags.split(',') } }],
 				},
 			],
 		}
-		const posts = await Post.find(query).sort({ createdAt: -1 })
+		let posts = await Post.aggregate([
+			{ $match: query },
+			{ $sort: { createdAt: -1 } },
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'creator',
+					foreignField: '_id',
+					as: 'creator',
+				},
+			},
+		])
+		posts = setCreator(posts)
 		res.status(200).json({ data: posts })
 	} catch (error) {
 		res.status(404).json({ message: error.message })
@@ -77,7 +109,7 @@ export const createPost = async (req, res) => {
 	try {
 		const newPost = new Post({
 			...post,
-			creator: mongoose.Types.ObjectId(req.userId),
+			creator: mongoose.Types.ObjectId(req.userId.padStart(24, '0')),
 			createdAt: new Date().toISOString(),
 		})
 		await newPost.save()
