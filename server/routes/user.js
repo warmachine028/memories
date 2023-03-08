0f9e52471c96373794f2c584c0d291efd4e12788
warmachine028 <warmachine028@outlook.com>
2023-03-08 11:55:26 +0530

    fix: added googleSignin function API route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signin, signup, updateDetails, getUserDetails, getUserPostsByType, forgotPassword, resetPassword } from '../controllers/user.js'
+import { signin, signup, updateDetails, getUserDetails, getUserPostsByType, forgotPassword, resetPassword, googleSignin } from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
+router.post('/googlesignin', googleSignin)
 router.post('/signup', signup)
 router.post('/forgotPassword', forgotPassword)
 router.post('/resetPassword', resetPassword)
