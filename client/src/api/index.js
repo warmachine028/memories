20d19cf51adf5f6aa9af82353569c45785eacabb
warmachine028 <warmachine028@outlook.com>
2023-03-11 14:40:15 +0530

    fix: fixed routes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiURL = ['https://memories-pritam-server.vercel.app', 'http://localhost:5000']
+const apiURL = ['https://memories-old-server.vercel.app', 'http://localhost:5000']
 const API = axios.create({ baseURL: apiURL[0] })
 
 API.interceptors.request.use((req) => {
