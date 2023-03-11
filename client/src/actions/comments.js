c20967077daba0d13e1cd915d5cc1366980ac9be
warmachine028 <warmachine028@outlook.com>
2023-03-11 13:46:06 +0530

    feat: added comments actions

---
+++ client/src/actions/comments.js
@@ -0,0 +1,37 @@
+import { CREATE_COMMENT, DELETE_COMMENT, FETCHED_COMMENTS, FETCHING_COMMENTS, FETCH_COMMENTS } from '../constants/actionTypes'
+import * as api from '../api'
+
+export const getComments = (postId) => async (dispatch) => {
+	try {
+		dispatch({ type: FETCHING_COMMENTS })
+		const { data } = await api.fetchComments(postId)
+		dispatch({ type: FETCH_COMMENTS, payload: data })
+		dispatch({ type: FETCHED_COMMENTS })
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const createComment = (comment, snackBar) => async (dispatch) => {
+	try {
+		dispatch({ type: FETCHING_COMMENTS })
+		const { data } = await api.createComment(comment)
+		dispatch({ type: CREATE_COMMENT, payload: data })
+		dispatch({ type: FETCHED_COMMENTS })
+		return data
+	} catch (error) {
+		snackBar('error', error)
+	}
+}
+
+export const deleteComment = (commentId, snackBar) => async (dispatch) => {
+	try {
+		dispatch({ type: FETCHING_COMMENTS })
+		await api.deleteComment(commentId)
+		dispatch({ type: DELETE_COMMENT, payload: commentId })
+		dispatch({ type: FETCHED_COMMENTS })
+	} catch (error) {
+		console.log(error)
+		snackBar('error', error)
+	}
+}
