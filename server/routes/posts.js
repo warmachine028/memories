db67cbd82baa75b65c0025cab92c4933d58d811d
warmachine028 <warmachine028@outlook.com>
2023-03-11 13:34:49 +0530

    perf: removed unncessary functions

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, deleteComment, commentPost, likePost } from '../controllers/posts.js'
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost } from '../controllers/posts.js'
 import auth from '../middleware/auth.js'
 
 const router = express.Router()
@@ -8,14 +8,8 @@ const router = express.Router()
 router.get('/', auth, getPosts)
 router.get('/search', auth, getPostsBySearch)
 router.get('/:id', auth, getPost)
-
 router.post('/', auth, createPost)
-router.post('/:id/commentPost', auth, commentPost)
-
 router.delete('/:id', auth, deletePost)
-
-router.patch('/:id/deleteComment', deleteComment)
 router.patch('/:id', auth, updatePost)
-router.patch('/:id/likePost', auth, likePost)
 
 export default router
