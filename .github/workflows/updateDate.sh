57f2a757cda84a9f2899cc3cfb60c1f13fc0af02
Pritam Kundu <warmachine028@outlook.com>
2023-05-01 10:51:45 +0530

    Update updateDate.sh

--- .github/workflows/updateDate.sh
+++ .github/workflows/updateDate.sh
@@ -2,9 +2,9 @@
 
 DaySuffix() {       
     case `date +%d` in    
-        1|21|31) echo "st";;
-        2|22)    echo "nd";;    
-        3|23)    echo "rd";;
+        01|21|31) echo "st";;
+        02|22)    echo "nd";;    
+        03|23)    echo "rd";;
         *)       echo "th";;
     esac                   
 }
