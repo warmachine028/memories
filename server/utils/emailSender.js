88429b67df77533a2e28ae1d63dabcea5fedac6a
warmachine028 <warmachine028@outlook.com>
2023-06-25 00:22:19 +0530

    fix: aded minor fixes

--- server/utils/emailSender.js
+++ server/utils/emailSender.js
@@ -20,7 +20,7 @@ export const sendEmail = (email, URL, res) => {
 		${URL}`,
 	}
 
-	transport.sendMail(mailOptions, (error, info) => {
+	return transport.sendMail(mailOptions, (error, info) => {
 		if (error) {
 			return res.status(500).json({ message: 'Email was not sent', error: error.message })
 		}
