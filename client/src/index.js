32a8a406351792e7541e8059909637b8a7fc9988
warmachine028 <warmachine028@outlook.com>
2023-07-24 23:04:46 +0530

    fix: added fixes for #53

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 import reducers from './reducers'
 import './index.css'
 import App from './App/App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { SnackbarProvider } from './contexts/SnackbarContext'
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)))
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+// serviceWorkerRegistration.register()
