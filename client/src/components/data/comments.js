e9a1eb5b5a428420b5ffd25a65088d1b6e3538e7
warmachine028 <warmachine028@outlook.com>
2023-08-27 02:18:43 +0530

    add: added temporary comments for checking

---
+++ client/src/components/data/comments.js
@@ -0,0 +1,170 @@
+const creator1 = {
+    _id: '640826f38431da1e123456778',
+    name: 'John Doe',
+    email: 'johnDoe@email.com',
+    avatar: {
+        accessoriesType: "Round",
+        clotheColor: "PastelYellow",
+        clotheType: "GraphicShirt",
+        eyeType: "WinkWacky",
+        eyebrowType: "DefaultNatural",
+        facialHairColor: "BrownDark",
+        facialHairType: "BeardMedium",
+        graphicType: "Deer",
+        hairColor: "Black",
+        hatColor: "Gray02",
+        mouthType: "Twinkle",
+        skinColor: "Yellow",
+        topType: "WinterHat3"
+    }, __v: 0
+}
+
+export const comments = [
+    {
+        "_id": "35fcb842-6bc4-4833-8baf-386efcd270af",
+        "createdAt": "2023-06-28T22:41:04.897Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Aperiam asperiores asperiores nulla beatae unde deleniti similique eos.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "252f1bf5-94d0-47f0-b7e5-7d54339b123b",
+        "createdAt": "2023-07-05T23:44:50.112Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Nobis dolor necessitatibus unde delectus blanditiis laudantium nostrum.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "db6d688e-5675-4a5e-a1b9-aa487d705533",
+        "createdAt": "2022-11-18T14:14:54.056Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Quam assumenda blanditiis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "23fb40b4-2fc6-48df-a6a2-b6014c425fad",
+        "createdAt": "2023-08-02T13:57:06.790Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Aut soluta incidunt quasi et minus maiores pariatur.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "14256fad-29b7-43cf-9a26-b57299ec375c",
+        "createdAt": "2023-02-11T11:25:59.717Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Eum molestiae dolores beatae nam.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "db840948-e418-4be0-94bf-003e52b90c89",
+        "createdAt": "2023-06-16T04:53:53.451Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Aliquam ab sequi adipisci consectetur quis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "b297f3f9-146f-4a80-b573-ba61b973cce4",
+        "createdAt": "2022-09-03T17:13:20.279Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Magni modi quibusdam ab provident.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "5af4f936-3d2d-452c-a9fb-22883f00c5d7",
+        "createdAt": "2022-10-12T16:56:31.227Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Perspiciatis officiis sit incidunt quas dignissimos cumque fugiat cupiditate facilis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "ca6daa2b-32dd-48c0-988c-613377141d82",
+        "createdAt": "2023-06-30T15:07:26.501Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Quos magnam sapiente occaecati aut voluptatum iusto voluptatum.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "994eb70f-4928-4207-91c5-c54ed39e2f0a",
+        "createdAt": "2023-04-19T14:59:41.258Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Architecto ipsum sequi consectetur dolore officia minus.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "caeb3fc4-90d3-4ae9-8dae-8b3766e22d2d",
+        "createdAt": "2023-03-17T13:09:19.624Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Aliquam voluptate officiis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "1a1abfd0-1fbe-4b92-850d-3baf09ca673d",
+        "createdAt": "2023-02-22T23:38:15.369Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "At facilis error beatae at distinctio quos necessitatibus quia.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "e30b4a4e-a3ae-4898-ae54-60e898b1426f",
+        "createdAt": "2023-07-13T01:21:56.148Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Illum repellendus minus tempore.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "8da95c55-c2be-4c36-94b5-c3c6fda10886",
+        "createdAt": "2023-04-08T11:22:28.102Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Sequi consequuntur aperiam vel id ad.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "e40724b4-72a8-4c67-a170-b6595b9a5cd4",
+        "createdAt": "2023-01-17T00:12:59.369Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Quia tempore necessitatibus vero laborum fugit odit facilis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    },
+    {
+        "_id": "58afd2a6-b5bd-44e1-bdc7-ed79043d9217",
+        "createdAt": "2023-07-24T03:21:35.443Z",
+        "creator": creator1,
+        "likes": [],
+        "message": "Commodi optio eum rem illum temporibus numquam debitis.",
+        "post": "64beed3d7ba580756e0c5fac",
+        "__v": 0
+    }
+]
+export const pages = 2
+export const page1 = comments.slice(0, 9)
+export const page2 = comments.slice(10,)
