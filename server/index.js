3f6d3a981ec7cf9762ae5e5a335b29de783db9f4
Pritam Kundu <warmachine028@outlook.com>
2023-08-29 12:58:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,12 +10,12 @@ dotenv.config()
 
 const app = express()
 
-// app.use(
-// 	rateLimit({
-// 		windowMs: 1 * 60 * 1000, // 1 minuite
-// 		max: 30,
-// 	})	
-// )
+app.use(
+    rateLimit({
+        windowMs: 1 * 60 * 1000, // 1 minuite
+ 	max: 30,
+    })	
+)
 app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
