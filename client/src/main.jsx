9783b809a84a83495ec0b14141ef0e37ef4cf688
warmachine028 <warmachine028@outlook.com>
2023-10-03 00:25:17 +0530

    feat: changed index.jsx to main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import './index.css'
+import App from './App/App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import { SnackbarProvider } from './contexts/SnackbarContext'
+
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk)))
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<SnackbarProvider>
+			<App />
+		</SnackbarProvider>
+	</Provider>
+)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
