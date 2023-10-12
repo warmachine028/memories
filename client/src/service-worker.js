c47dcf2f5094fe5cc95677bda10d4a84e1c9d438
Abi beniyal <42829681+beneyalraj@users.noreply.github.com>
2023-10-13 01:06:14 +0530

    Update unreachable and complex boolean

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -28,21 +28,11 @@ const fileExtensionRegexp = /[^/?]+\.[^/]+$/
 registerRoute(
 	// Return false to exempt requests from being fulfilled by index.html.
 	({ request, url }) => {
-		// If this isn't a navigation, skip.
-		if (request.mode !== 'navigate') {
-			return false
-		} // If this is a URL that starts with /_, skip.
-
-		if (url.pathname.startsWith('/_')) {
-			return false
-		} // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-		if (fileExtensionRegexp.test(url.pathname)){
-			return false
+		if (request.mode !== 'navigate' || url.pathname.startsWith('/_') || !fileExtensionRegexp.test(url.pathname)) {
+  			return false;
+		} else {
+  			return true;
 		}
-		// Return true to signal that we want to use the handler.
-
-		return true
 	},
 	createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 )
