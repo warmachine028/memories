f6abcf680630f0589049d8c780586960d0620c5c
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-09 02:31:07 +0530

    fix: vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { fileURLToPath } from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
