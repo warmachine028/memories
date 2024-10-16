7af64dbe2b2a30102404f8300e4f2807c8975e0c
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-10-16 22:41:26 +0530

    feat: added cron job for render server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { CronJob } from 'cron'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,3 +9,22 @@ app.use('/favicon.ico', express.static('public/favicon.ico')) //
 		res.send('Hello World!')
 	})
 	.listen(port, () => console.log(`Listening on port http://localhost:${port} ...`))
+
+// Create a cron job to ping the server every 20 minutes
+new CronJob(
+	'*/20 * * * *',
+	async () => {
+		try {
+			const response = await fetch(`https://memories-omm3.onrender.com`)
+			if (response.ok) {
+				console.log('Server pinged successfully')
+			} else {
+				console.error('Failed to ping server:', response.status, response.statusText)
+			}
+		} catch (error) {
+			console.error('Error pinging server:', error)
+		}
+	},
+	null,
+	true
+)
