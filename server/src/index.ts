73f020fd858388d11d74c30d6084d4966a437cc6
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-10-16 22:27:51 +0530

    feat: added favicon

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,8 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (_, res) => {
-	res.send('Hello World!')
-})
-
-app.listen(port, () => {
-	console.log(`Listening on port http://localhost:${port} ...`)
-})
+app.use('/favicon.ico', express.static('public/favicon.ico')) //
+	.get('/', (_, res) => {
+		res.send('Hello World!')
+	})
+	.listen(port, () => console.log(`Listening on port http://localhost:${port} ...`))
