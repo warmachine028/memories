c8047b710e78636050ee8384d5725aa51e36ac48
Pritam Kundu <warmachine028@users.noreply.github.com>
2024-11-23 23:54:35 +0530

    refactor: removed Account Settings

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,20 @@
 import { brand as logo } from '@/assets'
-import { Avatar, IconButton, ListItemText, ListItemIcon, ListItemButton, ListItem, List, Divider, Button, SwipeableDrawer, Box, ListItemAvatar } from '@mui/material'
+import {
+	Avatar,
+	IconButton,
+	ListItemText,
+	ListItemIcon,
+	ListItemButton,
+	ListItem,
+	List,
+	Divider,
+	Button,
+	SwipeableDrawer,
+	Box,
+	ListItemAvatar
+} from '@mui/material'
 import { Close, Dashboard, GitHub, Logout, Settings } from '@mui/icons-material'
-
-import ThemeSwitch from './ThemeSwitch'
+import { ThemeSwitch } from '.'
 import { Link } from 'react-router-dom'
 import { useAuth, useUser } from '@clerk/clerk-react'
 
@@ -12,26 +24,66 @@ const SideBar = ({ open, setOpen }) => {
 	const { signOut } = useAuth()
 
 	return (
-		<SwipeableDrawer open={open} onOpen={setOpen} onClose={closeDrawer} position="sticky">
-			<Box role="presentation" maxWidth={500} sx={{ width: { xs: '100vw' } }} bgcolor="background.paper" minHeight="100vh">
-				<Box display="flex" justifyContent="space-between" m={1} alignItems="center" top={0} zIndex={4} bgcolor="background.paper" position="sticky">
-					<Button LinkComponent={Link} to="/" sx={{ ':hover': { bgcolor: 'transparent' } }}>
+		<SwipeableDrawer
+			open={open}
+			onOpen={setOpen}
+			onClose={closeDrawer}
+			position="sticky"
+		>
+			<Box
+				role="presentation"
+				maxWidth={500}
+				sx={{ width: { xs: '100vw' } }}
+				bgcolor="background.paper"
+				minHeight="100vh"
+			>
+				<Box
+					display="flex"
+					justifyContent="space-between"
+					m={1}
+					alignItems="center"
+					top={0}
+					zIndex={4}
+					bgcolor="background.paper"
+					position="sticky"
+				>
+					<Button
+						LinkComponent={Link}
+						to="/"
+						sx={{ ':hover': { bgcolor: 'transparent' } }}
+					>
 						<img src="favicon.ico" alt="logo" width={40} />
 						<img src={logo} alt="logo" height={60} />
 					</Button>
-					<IconButton onClick={closeDrawer} sx={{ width: '50px', height: '50px' }}>
+					<IconButton
+						onClick={closeDrawer}
+						sx={{ width: '50px', height: '50px' }}
+					>
 						<Close />
 					</IconButton>
 				</Box>
 				{!user && (
 					<List>
 						<ListItem>
-							<Button variant="contained" fullWidth LinkComponent={Link} to="/login" onClick={closeDrawer}>
+							<Button
+								variant="contained"
+								fullWidth
+								LinkComponent={Link}
+								to="/login"
+								onClick={closeDrawer}
+							>
 								LOGIN
 							</Button>
 						</ListItem>
 						<ListItem>
-							<Button variant="outlined" fullWidth LinkComponent={Link} color="secondary" to="/signup" onClick={closeDrawer}>
+							<Button
+								variant="outlined"
+								fullWidth
+								LinkComponent={Link}
+								color="secondary"
+								to="/signup"
+								onClick={closeDrawer}
+							>
 								SIGN UP
 							</Button>
 						</ListItem>
@@ -44,19 +96,20 @@ const SideBar = ({ open, setOpen }) => {
 							<ListItemIcon>
 								<Avatar src={user.imageUrl} />
 							</ListItemIcon>
-							<ListItemText primary={user.fullName} secondary={user.email} />
+							<ListItemText
+								primary={user.fullName}
+								secondary={user.email}
+							/>
 						</ListItem>
 						<Divider />
-						<ListItemButton LinkComponent={Link} to="/user" onClick={closeDrawer}>
+						<ListItemButton
+							LinkComponent={Link}
+							to="/user"
+							onClick={closeDrawer}
+						>
 							<ListItemText primary="Dashboard" />
 							<ListItemIcon sx={{ minWidth: 0 }}>
 								<Dashboard />
-							</ListItemIcon>
-						</ListItemButton>
-						<ListItemButton LinkComponent={Link} to="/user/update" onClick={closeDrawer}>
-							<ListItemText primary="Account Settings" />
-							<ListItemIcon sx={{ minWidth: 0 }}>
-								<Settings />
 							</ListItemIcon>
 						</ListItemButton>
 						<ListItem>
@@ -87,7 +140,13 @@ const SideBar = ({ open, setOpen }) => {
 				<List>
 					<ListItem>
 						<ListItemAvatar>
-							<Avatar sx={{ bgcolor: 'primary.main' }} component={Link} to="https://github.com/warmachine028/memories" target="_blank" rel="noopener noreferrer">
+							<Avatar
+								sx={{ bgcolor: 'primary.main' }}
+								component={Link}
+								to="https://github.com/warmachine028/memories"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
 								<GitHub />
 							</Avatar>
 						</ListItemAvatar>
